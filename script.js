8594935bf9cb431af62fbdbe4a186d7edf65e056
Ameer_105 <ameerdamilola@gmail.com>
2022-06-10 02:09:22 +0100

    changed main background color

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 function changebackground(){
 	document.body.style.backgroundColor = 'white';
 }
+
 function changebackground2(){
 	document.body.style.backgroundColor = 'black';
+}
+
+function changebackground3(){
+	document.body.style.backgroundColor = 'red';
+}
+
+function changebackground4(){
+	document.body.style.backgroundColor = 'white';
+}
+
+function changebackground5(){
+	document.body.style.backgroundColor = 'white';
+}
+
+function changebackground6(){
+	document.body.style.backgroundColor = 'white';
+}
+
+function changebackground7(){
+	document.body.style.backgroundColor = 'white';
+}
+
+function changebackground8(){
+	document.body.style.backgroundColor = 'white';
+}
+
+function changebackground9(){
+	document.body.style.backgroundColor = 'white';
 }
