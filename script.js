e9344400aaaf1d53e4a33b2107538c13b61f4198
Ameer_105 <ameerdamilola@gmail.com>
2022-06-10 02:05:39 +0100

    black button working perfectly🎉

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function changebackground(){
-	document.body.style.backgroundColor = 'green';
+	document.body.style.backgroundColor = 'white';
+}
+function changebackground2(){
+	document.body.style.backgroundColor = 'black';
 }
